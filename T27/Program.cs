52bbcaf9be15a5b85e01398a311cf3e7d2eac56a
non sof <sofinalove2012@gmail.com>
2023-06-06 01:08:50 +0300

    in progress

--- T27/Program.cs
+++ T27/Program.cs
@@ -2,3 +2,20 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+Console.WriteLine("write a number");
+int a = Convert.ToInt32(Console.ReadLine());
+void summ (int s)
+{
+    string magic = Convert.ToString(s);
+    int count = magic.Length;
+    int result = 0;
+    for (int i = 0; i<count; i++)
+    {
+        result = result + magic[i];
+        Console.WriteLine (result);
+    }
+
+}
+
+summ (a);
